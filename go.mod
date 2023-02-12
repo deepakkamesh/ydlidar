34ff65bd40aaa03ae10783439485797e6e6c9b70
Larry Cawley <larrydcj@icloud.com>
2023-02-12 14:00:59 -0500

    adding tests

--- go.mod
+++ go.mod
@@ -2,9 +2,15 @@ module ydlidar
 
 go 1.17
 
-require go.bug.st/serial v1.5.0
+require (
+	github.com/stretchr/testify v1.8.1
+	go.bug.st/serial v1.5.0
+)
 
 require (
 	github.com/creack/goselect v0.1.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ ydlidar/lidarUtils_test.go
@@ -0,0 +1,24 @@
+package ydlidar
+
+import (
+	"github.com/stretchr/testify/assert"
+	"log"
+	"testing"
+)
+
+// TestGetHealthStatus tests the GetHealthStatus function.
+func TestDistanceCalculation(t *testing.T) {
+	// write a test to check the distance calculation
+
+	assert.Equal(t, 1, 1, "they should be equal")
+
+	log.Print("TestDistanceCalculation")
+}
+
+func TestIntensityCalculation(t *testing.T) {
+	log.Print("TestIntensityCalculation")
+}
+
+func TestAngleCalculation(t *testing.T) {
+	log.Print("TestAngleCalculation")
+}
